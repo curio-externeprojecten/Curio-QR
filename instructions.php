4fb52e6ab9336a2744b7760b7f10edc2e53638ef
machiel <lorddrone9@gmail.com>
2020-11-27 11:39:28 +0100

    start instructions page

--- instructions.php
+++ instructions.php
@@ -1,1 +1,100 @@
-{\rtf1}
+<?php
+
+//isloggedin check
+
+//require'header';
+require'backend/init.php';
+
+
+$instruction = selectOne(
+    "SELECT creator, title, description, username FROM instructions
+    LEFT JOIN users on instructions.creator = users.id
+    WHERE instructions.id = :id",
+     [":id" => $_GET['id']]);
+    
+$instructions = select(
+    "SELECT type, content FROM instructions_data
+    WHERE instruction_id = :id
+    ORDER BY instruction_order", 
+    [":id" => $_GET['id']]);
+
+$title = $instruction['title'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title><?=$title;?></title>
+</head>
+<body>
+
+
+<head>
+    
+</head>
+
+<div style='height:100px; background:black;'>
+    <h1 style="color:white;display:inline">Fake header INC.</h1>
+    <nav style='display:flex;justify-content:space-around;'>
+        <h2 style="color:white;">dashboard</h2>
+        <h2 style="color:white;">something</h2>
+        <h2 style="color:white;">user</h2>
+    </nav>
+</div>
+
+<div class='container' style='display:flex;border:1px black solid;padding:10px;background:gray;'>
+
+    <div class="instructions">
+        <div>
+            <!--instruction go here in div-->
+            <?php
+            foreach($instructions as $data){
+                if($data['type'] == "text"):
+                ?>    
+                <div class='instruction'>
+                    <p><?=$data['content'];?></p>
+                </div>
+                <?php
+                endif;
+            }
+            ?>
+        </div>
+        <!--add instruction function-->
+        <!--instructions are chunks of data text photo or video-->
+        <div class="addInstruction">
+            <button>add instruction</button>
+        </div>
+    </div>
+
+    <div class="instructionsControls">
+        <div class="instructionsInfo">
+            <div class='instructionsTitle'>
+                <h2><?=$title;?></h2>
+            </div>
+            <div class="instructionsCreator">
+                <!--creator info-->
+                <h3><?=$instruction['username'];?></h3>
+                <p style='padding:25px;'>image</p>
+            </div>
+            <div class="infoDetails">
+                <!--instruction details-->
+                <p><?=$instruction['description'];?></p>
+            </div>
+        </div>
+
+        <div class="controls" style='background:green;padding:20px;'>
+            <!--if high rank see controlls-->
+            <!--remove whole instruction-->
+            <div class="remove" style='margin:auto;width:100px'>
+                <button style='width:100px;height:50px;'>remove project</button>
+                <button style='width:100px;height:50px;'>get qr</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>
--- qr.sql
+++ qr.sql
@@ -1,227 +1,102 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 24, 2020 at 10:43 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server versie:                5.7.24 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Versie:              10.2.0.5599
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
---
--- Database: `qr`
---
 
--- --------------------------------------------------------
+-- Databasestructuur van qr wordt geschreven
+CREATE DATABASE IF NOT EXISTS `qr` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `qr`;
 
---
--- Table structure for table `instructions`
---
-
-CREATE TABLE `instructions` (
-  `id` int(11) NOT NULL,
+-- Structuur van  tabel qr.instructions wordt geschreven
+CREATE TABLE IF NOT EXISTS `instructions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `creator` int(11) NOT NULL,
-  `created_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `code` tinyblob,
   `title` varchar(30) NOT NULL,
-  `description` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `creator` (`creator`),
+  CONSTRAINT `instructions_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Data exporteren was gedeselecteerd
 
---
--- Table structure for table `instructions_data`
---
-
-CREATE TABLE `instructions_data` (
-  `id` int(11) NOT NULL,
+-- Structuur van  tabel qr.instructions_data wordt geschreven
+CREATE TABLE IF NOT EXISTS `instructions_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `instruction_id` int(11) NOT NULL,
   `instruction_order` int(11) NOT NULL,
   `type` enum('text','image','video','') NOT NULL,
-  `content` blob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `content` blob NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `instruction_id` (`instruction_id`),
+  CONSTRAINT `instructions_data_ibfk_1` FOREIGN KEY (`instruction_id`) REFERENCES `instructions` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Data exporteren was gedeselecteerd
 
---
--- Table structure for table `instructions_users`
---
-
-CREATE TABLE `instructions_users` (
-  `id` int(11) NOT NULL,
+-- Structuur van  tabel qr.instructions_users wordt geschreven
+CREATE TABLE IF NOT EXISTS `instructions_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `instruction_id` int(11) NOT NULL
+  `instruction_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `instruction_id` (`instruction_id`),
+  CONSTRAINT `instructions_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `instructions_users_ibfk_2` FOREIGN KEY (`instruction_id`) REFERENCES `instructions` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Data exporteren was gedeselecteerd
 
---
--- Table structure for table `users`
---
+-- Structuur van  tabel qr.users wordt geschreven
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `image` blob,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rank` enum('user','admin','superadmin','') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(11) NOT NULL,
-  `email` varchar(11) NOT NULL,
-  `password` varchar(11) NOT NULL,
-  `rank` enum('0','1','2','') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Data exporteren was gedeselecteerd
 
--- --------------------------------------------------------
-
---
--- Table structure for table `verification_answers`
---
-
-CREATE TABLE `verification_answers` (
-  `id` int(11) NOT NULL,
+-- Structuur van  tabel qr.verification_answers wordt geschreven
+CREATE TABLE IF NOT EXISTS `verification_answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `question_id` int(11) NOT NULL,
   `answer` varchar(255) NOT NULL,
-  `correct_answer` tinyint(1) NOT NULL
+  `correct_answer` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `verification_answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `verification_questions` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Data exporteren was gedeselecteerd
 
---
--- Table structure for table `verification_questions`
---
-
-CREATE TABLE `verification_questions` (
-  `id` int(11) NOT NULL,
+-- Structuur van  tabel qr.verification_questions wordt geschreven
+CREATE TABLE IF NOT EXISTS `verification_questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `instruction_id` int(11) NOT NULL,
-  `question` varchar(255) NOT NULL
+  `question` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `instruction_id` (`instruction_id`),
+  CONSTRAINT `verification_questions_ibfk_1` FOREIGN KEY (`instruction_id`) REFERENCES `instructions` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
+-- Data exporteren was gedeselecteerd
 
---
--- Indexes for table `instructions`
---
-ALTER TABLE `instructions`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `creator` (`creator`);
-
---
--- Indexes for table `instructions_data`
---
-ALTER TABLE `instructions_data`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `instruction_id` (`instruction_id`);
-
---
--- Indexes for table `instructions_users`
---
-ALTER TABLE `instructions_users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_id` (`user_id`),
-  ADD UNIQUE KEY `instruction_id` (`instruction_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `verification_answers`
---
-ALTER TABLE `verification_answers`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `question_id` (`question_id`);
-
---
--- Indexes for table `verification_questions`
---
-ALTER TABLE `verification_questions`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `instruction_id` (`instruction_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `instructions`
---
-ALTER TABLE `instructions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `instructions_data`
---
-ALTER TABLE `instructions_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `instructions_users`
---
-ALTER TABLE `instructions_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `verification_answers`
---
-ALTER TABLE `verification_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `verification_questions`
---
-ALTER TABLE `verification_questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `instructions`
---
-ALTER TABLE `instructions`
-  ADD CONSTRAINT `instructions_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `instructions_data`
---
-ALTER TABLE `instructions_data`
-  ADD CONSTRAINT `instructions_data_ibfk_1` FOREIGN KEY (`instruction_id`) REFERENCES `instructions` (`id`);
-
---
--- Constraints for table `instructions_users`
---
-ALTER TABLE `instructions_users`
-  ADD CONSTRAINT `instructions_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `instructions_users_ibfk_2` FOREIGN KEY (`instruction_id`) REFERENCES `instructions` (`id`);
-
---
--- Constraints for table `verification_answers`
---
-ALTER TABLE `verification_answers`
-  ADD CONSTRAINT `verification_answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `verification_questions` (`id`);
-
---
--- Constraints for table `verification_questions`
---
-ALTER TABLE `verification_questions`
-  ADD CONSTRAINT `verification_questions_ibfk_1` FOREIGN KEY (`instruction_id`) REFERENCES `instructions` (`id`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
