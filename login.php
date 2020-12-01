5219226844b9bd64a46cc91bd4c22c8a368b9f86
SterreVriens <Sterre.vriens03@gmail.com>
2020-12-01 11:34:01 +0100

    login

--- login.php
+++ login.php
@@ -1,17 +1,32 @@
-<?php
-require("header.php");
-?>
-        <main class="loginmain">
-            <div class="login">
-                <h1>Login</h1>
-                <form action="backend/controllers/users.php" method="post">
-                    <input type="hidden" name="formType" value="login">
-                    <input class="email" type="email" name="email" placeholder="email" id="email" required>
-                    <input class="password" type="password" name="password" placeholder="Password" id="password" required>
-                    <input type="submit" value="Login">
-                </form>
-            </div>
-        </main>
-<?php
-require('footer.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Login page</title>
+</head>
+<body>
+    <header>
+       <?php
+            require("header.php");
+        ?> 
+    </header>
+    <main class="loginmain">
+        <div class="login">
+         <h1>Login</h1>
+             <form action="backend/controllers/users.php" method="post">
+                <input type="hidden" name="formType" value="login">
+                <input class="email" type="email" name="email" placeholder="email" id="email" required>
+                <input class="password" type="password" name="password" placeholder="Password" id="password" required>
+             <input type="submit" value="Login">
+             </form>
+        </div>
+    </main>
+    <footer>
+        <?php
+            require('footer.php');
+        ?>
+    </footer>
+</body>
+</html>
