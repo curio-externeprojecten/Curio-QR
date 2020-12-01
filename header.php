b308be60101e04bdb78a9c18b59a39e1744598e8
Brae <brae30.09.2003@gmail.com>
2020-12-01 16:56:46 +0100

    header
    rough header part needs polish

--- header.php
+++ header.php
@@ -1,1 +1,25 @@
-{\rtf1}
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo&family=Quicksand:wght@500&display=swap" rel="stylesheet">
+
+    <title><?= $title = null ?></title>
+</head>
+<body>
+<header>
+    <div class="container">
+        <nav>
+            <div class="links">
+                <a href="index.php">Home Page</a>
+                <a href="login.php">Login</a>
+            </div>     
+            <div class="logo">
+                <img src="Images/curio.png" alt="curio logo">
+            </div>
+        </nav>
+    </div>
+</header>
