e19b47954865fdb012d8cf95e7c25098e69714ba
SterreVriens <Sterre.vriens03@gmail.com>
2020-12-08 13:23:45 +0100

    login styling done

--- login.php
+++ login.php
@@ -21,14 +21,35 @@
             </div>
         </div>
         <div class="login">
-         <h1>Login</h1>
-             <form action="backend/controllers/users.php" method="post">
-                <input type="hidden" name="formType" value="login">
-                <input class="email" type="email" name="email" placeholder="email" id="email" required>
-                <input class="password" type="password" name="password" placeholder="Password" id="password" required>
-             <input type="submit" value="Login">
-             </form>
+            <div class="container">
+              <div id="card">
+    <div id="card-content">
+      <div id="card-title">
+        <h2>LOGIN</h2>
+        <div class="underline-title"></div>
+      </div>
+      <form method="post" class="form" action="backend/controllers/users.php">
+        <input type="hidden" name="formType" value="login">
+        <label for="user-email" style="padding-top:13px">
+            &nbsp;Email
+          </label>
+        <input id="user-email" class="form-content" type="email" name="email" autocomplete="on" required />
+        <div class="form-border"></div>
+        <label for="user-password" style="padding-top:22px">&nbsp;Password
+          </label>
+        <input id="user-password" class="form-content" type="password" name="password" required />
+        <div class="form-border"></div>
+        <input id="submit-btn" type="submit" name="submit" value="login" />
+        <a href="#" id="signup">Don't have account yet?</a>
+      </form>
+    </div>
+  </div>
+            </div>
         </div>
+
+
+
+
     </main>
     <footer>
         <?php
