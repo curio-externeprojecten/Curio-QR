82f4df251ff41009ac928b303dd16d383c46dbc8
SterreVriens <Sterre.vriens03@gmail.com>
2021-01-05 11:24:41 +0100

    dashboard start

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,30 @@
-{\rtf1}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <header>
+     <?php
+     require("header.php");
+     ?> 
+    </header>
+    <main>
+        <div class="banner">
+            <div class="container">
+                <div class="bannertext">
+                    <h1>Dashboard</h1>
+                </div>
+             </div>
+         </div>
+    </main>
+    <footer>
+        <?php
+          require('footer.php');
+        ?>
+    </footer>
+</body>
+</html>
