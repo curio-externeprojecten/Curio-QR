381a413c408a3b366e1f494853f0891d724142fa
SterreVriens <Sterre.vriens03@gmail.com>
2021-01-08 11:37:46 +0100

    dashboard done

--- dashboard.php
+++ dashboard.php
@@ -36,6 +36,37 @@
                 </div>
              </div>
          </div>
+         <div class="maincontent">
+             <div class="container">
+                 <h3>Wat kan je hier doen?</h3>
+                 <div class="articles">
+                     <div class="article">
+                         <img src="backend/img/qr3.gif" alt="qr-code">
+                         <h4>Qr informatie pagina</h4>
+                         <p>Hier leggen we de werking uit van met maken van een nieuwe qr code, en leggen we uit hoe met maken van de pagina in elkaar zit.</p>
+                         <a href="">Naar de pagina ></a>
+                     </div>
+                     <div class="article">
+                         <img src="backend/img/qr2.gif" alt="qr-code">
+                         <h4>Qr pagina maken</h4>
+                         <p>Hier kan je dan ook daadwerkelijk een nieuwe qr pagina aanmaken als je de informatie hebt gelezen. Of als je die niet nodig hebt.</p>
+                         <a href="">Naar de pagina ></a>
+                     </div>
+                     <div class="article">
+                         <img src="backend/img/qr5.png" alt="People buying stuff">
+                         <h4>Admin Pagina</h4>
+                         <p>Deze pagina kan je alleen gebruiken als je een van de hogere admins bent. Hier kan je andere accounts beheren en andere account hogere admins maken. Ook kun je de Qr pagina's hier beheren</p>
+                         <a href="">Naar de pagina ></a>
+                     </div>
+                     <div class="article">
+                         <img src="backend/img/qr4.jpg" alt="Person at front desk">
+                         <h4>Jou account</h4>
+                         <p>Hier kan je jou accountgegevens zien en aanpassen wanneer gewenst.</p>
+                         <a href="">Naar de pagina ></a>
+                     </div>
+                 </div>
+             </div>
+         </div>
     </main>
     <footer>
         <?php
