259f185afdf3829fb1b08876b77e05e82dbce227
SterreVriens <Sterre.vriens03@gmail.com>
2021-01-15 09:33:59 +0100

    updates

--- index.php
+++ index.php
@@ -25,31 +25,6 @@
       </div>
 
     </div>
-<<<<<<< Updated upstream
-    <div class="container size">
-      <!-- <h1>QR Code generator</h1> -->
-      <p>De applicatie werkt als volgt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-        Vero illo adipisci officia reprehenderit facere dolore nihil deserunt sapiente nesciunt illum corrupti voluptatibus mollitia impedit culpa esse, 
-        rem fuga enim molestiae voluptates quas voluptas quibusdam sint facilis? Repellendus non illum excepturi asperiores doloremque praesentium quia perferendis obcaecati. 
-        Aliquam vitae debitis similique repellat esse tempore aspernatur aperiam! Quidem, harum. Harum odio vero provident quis, ut, 
-        similique modi nam nostrum totam nulla maxime cumque soluta atque doloribus vitae temporibus ea? Asperiores, aperiam qui?</p>
-      <p>De applicatie werkt als volgt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-        Vero illo adipisci officia reprehenderit facere dolore nihil deserunt sapiente nesciunt illum corrupti voluptatibus mollitia impedit culpa esse, 
-        rem fuga enim molestiae voluptates quas voluptas quibusdam sint facilis? Repellendus non illum excepturi asperiores doloremque praesentium quia perferendis obcaecati. 
-        Aliquam vitae debitis similique repellat esse tempore aspernatur aperiam! Quidem, harum. Harum odio vero provident quis, ut, 
-        similique modi nam nostrum totam nulla maxime cumque soluta atque doloribus vitae temporibus ea? Asperiores, aperiam qui?</p>
-        <img src="https://picsum.photos/1200/150">
-      <p>De applicatie werkt als volgt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-        Vero illo adipisci officia reprehenderit facere dolore nihil deserunt sapiente nesciunt illum corrupti voluptatibus mollitia impedit culpa esse, 
-        rem fuga enim molestiae voluptates quas voluptas quibusdam sint facilis? Repellendus non illum excepturi asperiores doloremque praesentium quia perferendis obcaecati.
-         Aliquam vitae debitis similique repellat esse tempore aspernatur aperiam! Quidem, harum. Harum odio vero provident quis, ut, 
-         similique modi nam nostrum totam nulla maxime cumque soluta atque doloribus vitae temporibus ea? Asperiores, aperiam qui?</p>
-      <p>De applicatie werkt als volgt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-        Vero illo adipisci officia reprehenderit facere dolore nihil deserunt sapiente nesciunt illum corrupti voluptatibus mollitia impedit culpa esse, 
-        rem fuga enim molestiae voluptates quas voluptas quibusdam sint facilis? Repellendus non illum excepturi asperiores doloremque praesentium quia perferendis obcaecati. 
-        Aliquam vitae debitis similique repellat esse tempore aspernatur aperiam! Quidem, harum. Harum odio vero provident quis, ut, 
-        similique modi nam nostrum totam nulla maxime cumque soluta atque doloribus vitae temporibus ea? Asperiores, aperiam qui?</p> 
-=======
     <div class="container">
       <div class="articles">
         <article>
@@ -66,7 +41,6 @@
         <article><h2>Hoe gaat deze applicatie te werk.</h2>
           <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero illo adipisci officia reprehenderit facere dolore nihil deserunt sapiente nesciunt illum corrupti voluptatibus mollitia impedit culpa esse, rem fuga enim molestiae voluptates quas voluptas quibusdam sint facilis? Repellendus non illum excepturi asperiores doloremque praesentium quia perferendis obcaecati. Aliquam vitae debitis similique repellat esse tempore aspernatur aperiam! Quidem, harum. Harum odio vero provident quis, ut, similique modi nam nostrum totam nulla maxime cumque soluta atque doloribus vitae temporibus ea? Asperiores, aperiam qui?</p></article>
       </div>
->>>>>>> Stashed changes
     </div>
   </main>
   <footer>
