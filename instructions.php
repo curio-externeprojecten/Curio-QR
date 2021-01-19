aafcbd10b6db1054508b6ca2a4aa4910107f980a
machiel <lorddrone9@gmail.com>
2021-01-19 11:21:47 +0100

    instructions

--- instructions.php
+++ instructions.php
@@ -1,11 +1,6 @@
 <?php
-<<<<<<< Updated upstream
 require __DIR__.'\header.php';
 require __DIR__.'\backend/init.php';
-=======
-//require'header';
-require'backend/init.php';
->>>>>>> Stashed changes
 
 $id = decrypt($_GET['id']);
 
@@ -14,17 +9,15 @@ if(isset($_SESSION['userId'])){
     $user = selectOne(
         //username
         //rank user/admin/superadmin
-        "SELECT username, rank FROM users WHERE id = :id",
-        [":id" => $_SESSION['userId']]
+        "SELECT id, username, rank FROM users WHERE id = :id",
+        [":id" => decryptUser($_SESSION['userId'])]
     );
+}else{
+    $user = false;
 }
 
 $instruction = selectOne(
-<<<<<<< Updated upstream
     "SELECT creator, title, description, username, code FROM instructions
-=======
-    "SELECT creator, title, description, username FROM instructions
->>>>>>> Stashed changes
     LEFT JOIN users on instructions.creator = users.id
     WHERE instructions.id = :id",
      [":id" => $id]);
@@ -32,13 +25,6 @@ $instruction = selectOne(
 if(!$instruction){
     header("Location: index.php"); 
 }
-$instructions = select(
-    "SELECT type, content FROM instructions_data
-    WHERE instruction_id = :id
-    ORDER BY instruction_order", 
-    [":id" => $id]);
-
-<<<<<<< Updated upstream
 ?>
 
 <div class='instructionContainer'>
@@ -58,8 +44,11 @@ $instructions = select(
             document.getElementById("removeInstruct").style.display = "block";
         }
         
-        function addUser(){
-            //do something
+        function changeUser(){
+            document.getElementById("userchanger").style.display = "block";
+        }
+        function closeChangeUser(){
+            document.getElementById("userchanger").style.display = "none";
         }
     </script>
 
@@ -71,25 +60,64 @@ $instructions = select(
             <div class="instructionsCreator">
                 <!--creator info-->
                 <h3><?=$instruction['username'];?></h3>
-                <p style='padding:25px;'>image</p>
             </div>
             <div class="infoDetails">
                 <!--instruction details-->
                 <p><?=$instruction['description'];?></p>
             </div>
         </div>
-
+        
         <div class="controls">
             <!--if high rank see controlls-->
             <!--remove whole instruction-->
             <div class="remove">
-                <button>Voeg {gebruiker} toe</button>
-                <button onclick="confirm()">Verwijder Instructies</button>
-                <form action="backend/controllers/informationController.php" method="POST">
-                    <input type="hidden" name="formType" value="Delete">
-                    <input type="hidden" name="id" value="<?=$_GET['id']?>">
-                    <input id="removeInstruct" type="submit" value="VERWIJDER">
-                </form>
+                <?php
+                $isAdmin = false;
+                $isLocalAdmin = false;
+                $isLocalSuperuser = false;
+
+                if($user != false){
+                    $userCheck = selectOne("SELECT rank FROM instructions_users where instruction_id = :id AND user_id = :userid", [":id" => $id, ":userid" => $user['id']]);
+                    if($userCheck['rank'] == "admin"){
+                        $isLocalAdmin = true;
+                    }
+                    if($userCheck['rank'] == "superadmin"){
+                        $isLocalSuperuser = true;
+                    }
+                    if($user['rank'] == "admin" || $user['rank'] == "superadmin"){
+                        $isAdmin = true;
+                    }
+                }
+                if($isAdmin || $isLocalSuperuser):
+                ?>
+                    <button onclick="changeUser()">verander gemachtigde gebruiker</button>
+                    <div class="addWindow userchanger" id="userchanger">
+                        <button onclick="closeChangeUser()">X</button>
+                        <form id="userform"action="backend/controllers/informationController.php" method="POST">
+                        <input type="hidden" name="formtype" value="changeuser">
+                        <input type="hidden" name="id" value="<?=$_GET['id']?>">
+                        <select name="rank" form="userform" >
+                            <option value="user">gebruiker</option>
+                            <option value="admin">admin</option>
+                        </select>
+                        <label for="selecteduser">gebruikers email: </label>
+                        <input type="text" name="selecteduser" required>
+                        <input type="submit" value="verander gebruiker">
+                        </form>
+                    </div>
+                <?php
+                endif;
+                if($isAdmin || $isLocalSuperuser):
+                ?>
+                    <button onclick="confirm()">Verwijder Instructies</button>
+                    <form action="backend/controllers/informationController.php" method="POST">
+                        <input type="hidden" name="formtype" value="Delete">
+                        <input type="hidden" name="id" value="<?=$_GET['id']?>">
+                        <input id="removeInstruct" type="submit" value="VERWIJDER">
+                    </form>
+                <?php
+                endif;
+                ?>
                 <button onclick="openqr()">genereer QRcode</button>
             </div>
             
@@ -100,112 +128,11 @@ $instructions = select(
         </div>
     </div>
     <div class="instructions">
-        <div>
-            <?php
-            //iframe?
-=======
-$title = $instruction['title'];
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title><?=$title;?></title>
-</head>
-<body>
-
-
-<head>
-    
-</head>
-
-<div style='height:100px; background:black;'>
-    <h1 style="color:white;display:inline">Fake header INC.</h1>
-    <nav style='display:flex;justify-content:space-around;'>
-        <h2 style="color:white;">dashboard</h2>
-        <h2 style="color:white;">something</h2>
-        <h2 style="color:white;">user</h2>
-    </nav>
-</div>
-
-<div class='container'>
-
-    <div class="instructions">
-        <div>
-            <!--instruction go here in div-->
-            <?php
->>>>>>> Stashed changes
-            foreach($instructions as $data){
-                if($data['type'] == "text"):
-                ?>    
-                    <div class='instruction'>
-                        <p><?=$data['content'];?></p>
-                    </div>
-                <?php
-                endif;
-                if($data['type'] == "image"):
-                ?>
-                    <div class="instruction">
-                        <img src="<?=$data['content'];?>">
-                    </div>
-                <?php
-                endif;
-                if($data['type'] == "video"):
-                ?>
-                    <div class="instruction">
-                        <video src="<?=$data['content'];?>" controls></video>
-                    </div>
-                <?php
-                endif;
-            }
-            ?>
-        </div>
-        <!--add instruction function-->
-        <!--instructions are chunks of data text photo or video-->
-        <div class="addInstruction">
-<<<<<<< Updated upstream
-            <button>voeg instructie toe</button>
-        </div>
+        <iframe src="instruction.php?id=<?=$_GET['id']?>" frameborder="0" allowfullscreen>
+            
+        </iframe>
     </div>
 </div>
 <?php
 require __DIR__.'\footer.php';
 ?>
-=======
-            <button>add instruction</button>
-        </div>
-    </div>
-
-    <div class="instructionsControls">
-        <div class="instructionsInfo">
-            <div class='instructionsTitle'>
-                <h2><?=$title;?></h2>
-            </div>
-            <div class="instructionsCreator">
-                <!--creator info-->
-                <h3><?=$instruction['username'];?></h3>
-                <p style='padding:25px;'>image</p>
-            </div>
-            <div class="infoDetails">
-                <!--instruction details-->
-                <p><?=$instruction['description'];?></p>
-            </div>
-        </div>
-
-        <div class="controls">
-            <!--if high rank see controlls-->
-            <!--remove whole instruction-->
-            <div class="remove">
-                <button>remove project</button>
-                <button>get qr</button>
-            </div>
-        </div>
-
-    </div>
-</div>
-
-</body>
-</html>
->>>>>>> Stashed changes
