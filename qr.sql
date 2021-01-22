05fed327572324192f2de3c34dd0ce5e2cc100db
machiel <lorddrone9@gmail.com>
2021-01-22 09:36:34 +0100

    sql update

--- qr.sql
+++ qr.sql
@@ -21,13 +21,13 @@ CREATE TABLE IF NOT EXISTS `instructions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `creator` int(11) NOT NULL,
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `code` tinyblob,
+  `code` blob,
   `title` varchar(30) NOT NULL,
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `creator` (`creator`),
   CONSTRAINT `instructions_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 -- Data exporteren was gedeselecteerd
 
@@ -37,11 +37,11 @@ CREATE TABLE IF NOT EXISTS `instructions_data` (
   `instruction_id` int(11) NOT NULL,
   `instruction_order` int(11) NOT NULL,
   `type` enum('text','image','video','') NOT NULL,
-  `content` blob NOT NULL,
+  `content` mediumblob NOT NULL,
   PRIMARY KEY (`id`),
   KEY `instruction_id` (`instruction_id`),
   CONSTRAINT `instructions_data_ibfk_1` FOREIGN KEY (`instruction_id`) REFERENCES `instructions` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 -- Data exporteren was gedeselecteerd
 
@@ -50,12 +50,13 @@ CREATE TABLE IF NOT EXISTS `instructions_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `instruction_id` int(11) NOT NULL,
+  `rank` enum('user','admin','superadmin') NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `instruction_id` (`instruction_id`),
   CONSTRAINT `instructions_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `instructions_users_ibfk_2` FOREIGN KEY (`instruction_id`) REFERENCES `instructions` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- Data exporteren was gedeselecteerd
 
@@ -66,38 +67,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   `image` blob,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `rank` enum('user','admin','superadmin','') NOT NULL,
+  `rank` enum('user','admin','superadmin') NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
--- Data exporteren was gedeselecteerd
-
--- Structuur van  tabel qr.verification_answers wordt geschreven
-CREATE TABLE IF NOT EXISTS `verification_answers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `question_id` int(11) NOT NULL,
-  `answer` varchar(255) NOT NULL,
-  `correct_answer` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `question_id` (`question_id`),
-  CONSTRAINT `verification_answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `verification_questions` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Data exporteren was gedeselecteerd
-
--- Structuur van  tabel qr.verification_questions wordt geschreven
-CREATE TABLE IF NOT EXISTS `verification_questions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `instruction_id` int(11) NOT NULL,
-  `question` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `instruction_id` (`instruction_id`),
-  CONSTRAINT `verification_questions_ibfk_1` FOREIGN KEY (`instruction_id`) REFERENCES `instructions` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- Data exporteren was gedeselecteerd
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-instructions
